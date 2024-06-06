a08e80893de193fe60b9f87c86e3e519d8df3dc0
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-06 10:44:39 -0500

    + moved to mainPages

--- _config/mainPages.js
+++ _config/mainPages.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+    notificationsIndex: {
+        permission: 'notification.notifications.manage',
+        activated: true,
+        path: '/notificaciones/index',
+        name: 'notification.admin.notificationIndex',
+        //crud: import('@imagina/qnotification/_crud/notifications'),
+        layout: () => import('@imagina/qsite/_layouts/master'),
+        page: () => import('@imagina/qnotification/_pages/admin/notificationsIndex'),
+        title: 'Notifications',
+        icon: 'fa-light fa-panel-ews',
+        authenticated: true,
+        subHeader: {
+          refresh: true,
+        }
+      }
+}
