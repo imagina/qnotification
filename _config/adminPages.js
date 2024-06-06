ba9b22ab5904df88920bd4fc73970ef5c327445d
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-06 12:02:21 -0500

    - removed from admin

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -42,20 +42,5 @@ export default {
     subHeader: {
       refresh: true,
     }
-  },
-  notificationsIndex: {
-    permission: 'notification.notifications.manage',
-    activated: true,
-    path: '/notificaciones/index',
-    name: 'notification.admin.notificationIndex',
-    //crud: import('@imagina/qnotification/_crud/notifications'),
-    layout: () => import('@imagina/qsite/_layouts/master'),
-    page: () => import('@imagina/qnotification/_pages/admin/notificationsIndex'),
-    title: 'Notifications',
-    icon: 'fa-light fa-panel-ews',
-    authenticated: true,
-    subHeader: {
-      refresh: true,
-    }
-  }
+  }  
 }
