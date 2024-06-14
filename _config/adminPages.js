f71700ef9da5ad8817bad69146eb59af1f824d28
msolanogithub <msolano@imaginacolombia.com>
2024-06-14 14:38:07 -0500

    updated the notification panel CRUD

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -23,24 +23,10 @@ export default {
     layout: () => import('@imagina/qsite/_layouts/master'),
     page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
     title: 'notification.cms.sidebar.adminPanel',
-    icon: 'fa-light fa-panel-ews',
+    icon: 'fa-light fa-bell',
     authenticated: true,
     subHeader: {
       refresh: true,
     }
-  }, 
-  setting: {
-    permission: 'notification.providers.manage',
-    activated: true,
-    path: '/notificaciones/configuracion',
-    name: 'notification.admin.providers',
-    layout: () => import('@imagina/qsite/_layouts/master'),
-    page: () => import('@imagina/qnotification/_pages/admin/setting/index'),
-    title: 'notification.cms.sidebar.adminProvider',
-    icon: 'fa-light fa-bell-plus',
-    authenticated: true,
-    subHeader: {
-      refresh: true,
-    }
-  }  
+  }
 }
--- _crud/notifications.vue
+++ _crud/notifications.vue
@@ -4,7 +4,8 @@
 export default {
   data() {
     return {
-      crudId: this.$uid()
+      crudId: this.$uid(),
+      users: []
     };
   },
   computed: {
@@ -22,10 +23,24 @@ export default {
         },
         read: {
           columns: [
-            { name: 'id', label: this.$tr('isite.cms.form.id'), field: 'id', style: 'width: 50px' },
-            { name: 'title', label: this.$tr('isite.cms.form.title'), field: 'title', align: 'rigth' },
-            { name: 'recipient', label: this.$tr('isite.cms.label.recipient'), field: 'recipient', align: 'rigth' },
-            { name: 'message', label: this.$tr('notification.cms.label.message'), field: 'message', align: 'rigth' },
+            {name: 'id', label: this.$tr('isite.cms.form.id'), field: 'id', style: 'width: 50px'},
+            {name: 'type', label: this.$tr('isite.cms.label.provider'), field: 'type', align: 'rigth'},
+            {
+              name: 'user', label: this.$tr('isite.cms.label.user'), field: 'user', align: 'rigth',
+              format: val => val?.fullName || '-'
+            },
+            {
+              name: 'recipient', label: this.$tr('isite.cms.label.recipient'), field: 'recipientUser', align: 'rigth',
+              format: (val, row) => row.recipientUser?.email || row.recipient
+            },
+            {
+              name: 'title', label: this.$tr('isite.cms.form.title'), field: 'title', align: 'rigth',
+              classes: 'ellipsis', style: 'max-width : 250px'
+            },
+            {
+              name: 'message', label: this.$tr('notification.cms.label.message'), field: 'message', align: 'rigth',
+              classes: 'ellipsis', style: 'max-width : 250px'
+            },
             {
               name: 'created_at', label: this.$tr('isite.cms.form.createdAt'), field: 'createdAt', align: 'right',
               format: val => val ? this.$trd(val) : '-'
@@ -33,23 +48,57 @@ export default {
             {
               name: 'updated_at', label: this.$tr('isite.cms.form.updatedAt'), field: 'updatedAt', align: 'right',
               format: val => val ? this.$trd(val) : '-'
-            },
-            { name: 'actions', label: this.$tr('isite.cms.form.actions'), align: 'right' }
-          ]
+            }
+          ],
+          requestParams: {include: 'user,recipientUser'}
         },
         update: false,
-        delete: true,
+        delete: false,
         formLeft: {
-          id: { value: '' },
-          type: {
-            value: 'email',
+          id: {value: ''},
+          providers: {
+            value: [],
             type: 'select',
             required: true,
             props: {
-              label: `${this.$tr('isite.cms.form.type')}*`,
+              label: `${this.$trp('isite.cms.label.provider')}*`,
+              multiple: true,
+              useChips: true,
               options: [
                 {label: 'Email', value: 'email'},
+                {
+                  label: 'Broadcast',
+                  value: 'broadcast',
+                  sublabel: this.$tr('notification.cms.helpTextBroadcastOption')
+                },
+                {
+                  label: this.$tr('notification.cms.isImportant'),
+                  value: 'broadcastImportant',
+                  sublabel: this.$tr('notification.cms.isImportantHelpText')
+                },
               ],
+            }
+          },
+          users: {
+            value: [],
+            type: 'select',
+            props: {
+              label: `${this.$trp('isite.cms.label.recipient')}*`,
+              multiple: true,
+              useChips: true,
+              useInput: true,
+              rules: [
+                val => !!val?.length || this.$tr('isite.cms.message.fieldRequired')
+              ],
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.quser.users',
+              filterByQuery: true,
+              select: {
+                label: 'email',
+                id: item => `${item.id}::${item.email}`,
+                sublabel: 'fullName'
+              }
             }
           },
           title: {
@@ -58,22 +107,6 @@ export default {
             required: true,
             props: {
               label: `${this.$tr('isite.cms.form.title')}*`
-            }
-          },
-          email: {
-            value: [],
-            type: 'treeSelect',
-            fakeFieldName: 'to',
-            props: {
-              label: `${this.$trp('isite.cms.label.recipient')}*`,
-              multiple: true,
-              rules: [
-                val => !!val?.length || this.$tr('isite.cms.message.fieldRequired')
-              ],
-            },
-            loadOptions: {
-              apiRoute: 'apiRoutes.quser.users',
-              select: {label: 'email', id: 'email'}
             }
           },
           message: {
@@ -85,22 +118,59 @@ export default {
                 val => !!val || this.$tr('isite.cms.message.fieldRequired')
               ],
             }
-          },
-          saveInDatabase: {
-            value: '0',
-            fakeFieldName: 'setting',
-            name: "saveInDatabase",
-            type: 'select',
-            required: true,
-            props: {
-              label: 'Save in database',
-              options: [
-                { label: 'enabled', value: '1' },
-                { label: 'disabled', value: '0' },
-              ],
-            },
           }
-        }
+        },
+        formRight: {
+          link: {
+            value: null,
+            type: 'input',
+            props: {
+              label: `URL`
+            }
+          },
+          mediasSingle: {
+            name: 'mediasSingle',
+            value: {},
+            type: 'media',
+            props: {
+              label: this.$tr('isite.cms.form.image'),
+              zone: 'mainimage',
+              entity: "Modules\\Notification\\Entities\\Notification",
+              entityId: null
+            }
+          }
+        },
+        getDataForm: (data, typeForm) => {
+          return new Promise((resolve, reject) => {
+            //Init the formData
+            let formData = {
+              userId: this.$store.state.quserAuth.userId,
+              source: config('app.mode'),
+              setting: {saveInDatabase: '1'},
+              title: data.title,
+              message: data.message,
+              mediasSingle: data.mediasSingle,
+              link: data.link,
+              to: {},
+              options: {}
+            }
+
+            //Set the recipients by provider
+            let providerUserToNotify = {
+              broadcast: data.users.map(item => item.split('::')[0]),
+              email: data.users.map(item => item.split('::')[1])
+            }
+            data.providers.forEach(provider => {
+              if (provider == 'broadcastImportant') {
+                provider = 'broadcast'
+                formData.options.isImportant = true
+              }
+              formData.to[provider] = providerUserToNotify[provider]
+            })
+
+            resolve(formData)
+          })
+        },
       };
     }
   }
